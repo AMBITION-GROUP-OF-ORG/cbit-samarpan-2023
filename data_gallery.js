286ecb1eb65469db436f18048d00808f9e616c8d
JAYVARDHAN REDDY <131672947+it-is-jayvardhan@users.noreply.github.com>
2023-05-11 06:09:48 +0530

    Updated data_gallery.js

--- data_gallery.js
+++ data_gallery.js
@@ -1,2 +1,3 @@
-export const gallery_data=[{link:"https://github.com/AMBITION-GROUP-OF-ORG/cbit-samarpan-2023/blob/main/gallery-samarpan/im1.jpg?raw=true"},
+const gallery_data=[{link:"https://github.com/AMBITION-GROUP-OF-ORG/cbit-samarpan-2023/blob/main/gallery-samarpan/im1.jpg?raw=true"},
 				{link:"https://github.com/AMBITION-GROUP-OF-ORG/cbit-samarpan-2023/blob/main/gallery-samarpan/im1.jpg?raw=true"}]; 
+export default gallery_data;
